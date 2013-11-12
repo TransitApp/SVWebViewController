d172cae50995df6871a1eff4351c47bb278ff277
Sam Vermette <samvermette@gmail.com>
2013-11-12 00:00:26 -0500

    Do a little self-promotion in demo instead of old Rebecca.

--- Demo/Classes/ViewController.m
+++ Demo/Classes/ViewController.m
@@ -13,14 +13,14 @@
 
 
 - (void)pushWebViewController {
-    NSURL *URL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/Friday_(Rebecca_Black_song)"];
+    NSURL *URL = [NSURL URLWithString:@"http://samvermette.com"];
 	SVWebViewController *webViewController = [[SVWebViewController alloc] initWithURL:URL];
 	[self.navigationController pushViewController:webViewController animated:YES];
 }
 
 
 - (void)presentWebViewController {
-	NSURL *URL = [NSURL URLWithString:@"http://en.wikipedia.org/wiki/Friday_(Rebecca_Black_song)"];
+	NSURL *URL = [NSURL URLWithString:@"http://samvermette.com"];
 	SVModalWebViewController *webViewController = [[SVModalWebViewController alloc] initWithURL:URL];
 	webViewController.modalPresentationStyle = UIModalPresentationPageSheet;
     webViewController.availableActions = SVWebViewControllerAvailableActionsOpenInSafari | SVWebViewControllerAvailableActionsOpenInChrome | SVWebViewControllerAvailableActionsCopyLink | SVWebViewControllerAvailableActionsMailLink;
